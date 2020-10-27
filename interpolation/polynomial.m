a0a3b5b3a37a42ea88a0018c9eade5d2d0194c99
Irelia <1289656383@qq.com>
2020-10-27 11:08:25 -0400

    Create polynomial.m

---
+++ interpolation/polynomial.m
@@ -0,0 +1,28 @@
+NN = [5 10 15 20];
+
+dx    = 0.01;
+xeval = xpts(1):dx:xpts(end); 
+yeval = 1 ./ (1 + 25 .* (xeval .^ 2));
+
+i = 0;
+
+for N = NN
+    
+    i = i + 1;
+    xpts = linspace(-1,1,N);
+    ypts = 1 ./ (1 + 25 .* (xpts .^ 2));
+    poly = lagrange(xpts,ypts,xeval);
+    
+    l_2 = sum((yeval - poly) .^ 2) ^ 0.5;
+    l_inf = max(yeval - poly);
+    disp(['N = ', num2str(N)]);
+    disp(['l-2 = ', num2str(l_2)]);
+    disp(['l-inf = ', num2str(l_inf)]);
+    
+    subplot(2,2,i);
+    plot(xeval,poly,'-b','LineWidth',2); hold on;
+    plot(xpts,ypts,'.k','MarkerSize',20); hold on;
+    plot(xeval,yeval,'-r','LineWidth',2); hold off;
+    xlabel('x'), ylabel('y'), title(N);
+    legend('P(x)','points','f(x)');
+end
